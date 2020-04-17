444a80330025ec98b2a42b690eea2e1b30968b64
sushant4191 <63559772+sushant4191@users.noreply.github.com>
2020-04-18 01:47:09 +0545

    Add files via upload

---
+++ Print sentemce.py
@@ -0,0 +1,5 @@
+#Printing Hello world using python
+s=input("Enter the sentence: ")
+n=int(input("Enter the number of times:"))
+for i in range(n):
+    print(s)
---
+++ Sum of two numbers.c
@@ -0,0 +1,13 @@
+//Sum of two numbers
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("\nEnter the first number:");
+	scanf("%d",&a);
+	printf("\nEnter the second number:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Sum of %d + %d = %d", a,b,c);
+	return 0;
+}
